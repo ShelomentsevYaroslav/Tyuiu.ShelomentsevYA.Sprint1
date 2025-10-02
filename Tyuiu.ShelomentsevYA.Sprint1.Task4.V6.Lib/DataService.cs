2e4e54e5db923fd06807fe45d92e675c45f7a91c
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-02 12:11:51 +0500

    Task4

---
+++ Tyuiu.ShelomentsevYA.Sprint1.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.ShelomentsevYA.Sprint1.Task4.V6.Lib
+{
+    public class DataService : ISprint1Task4V6
+    {
+
+        public double Calculate(double x, double y)
+        {
+            double z = (Math.Abs(Math.Pow(y, 2) - x)) / (x * y);
+            return Math.Round(z, 3, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ Tyuiu.ShelomentsevYA.Sprint1.Task4.V6/Program.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using Tyuiu.ShelomentsevYA.Sprint1.Task4.V6.Lib;
+namespace Tyuiu.ShelomentsevYA.Sprint1.Task4.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите число X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.WriteLine("Введите число Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = ds.Calculate(x, y);
+            Console.WriteLine("Результат формулы: " + a );
+            Console.ReadLine();
+        }
+    }
+}
