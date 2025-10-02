5fa060f98b2f437dce99f071ceca9e06764be9e8
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-02 20:05:28 +0500

    Task5

---
+++ Tyuiu.ShelomentsevYA.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShelomentsevYA.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.ShelomentsevYA.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+
+        {
+            DataService ds = new DataService();
+            int x = 13257;
+            int res = ((tyuiu.cources.programming.interfaces.Sprint1.ISprint1Task5V4)ds).SecondsToHours(x);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.ShelomentsevYA.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using Tyuiu.ShelomentsevYA.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.ShelomentsevYA.Sprint1.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите число секунд:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            int h = ((tyuiu.cources.programming.interfaces.Sprint1.ISprint1Task5V4)ds).SecondsToHours(x);
+            Console.WriteLine("Полное количество часов: " + h);
+
+
+            Console.ReadLine();
+        }
+    }
+}
